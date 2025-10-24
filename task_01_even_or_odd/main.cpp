57d1f7f5bbd107b2d5756aee1181018df28587a1
elenastasova9-hash <elenastasova9@gmail.com>
2025-10-24 08:35:28 +0200

    zatiaľ

--- task_01_even_or_odd/main.cpp
+++ task_01_even_or_odd/main.cpp
@@ -1,10 +1,17 @@
 
 #include <iostream>
+using namespace std;
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     long long n;
-    if (!(std::cin >> n)) return 0;
-    // TODO: print "even" or "odd"
+
+    std::cout << "Nacital cislo:";
+
+    std::cin >> n;
+
+    if (!(n)) {
+        return 0;
+    }
     return 0;
 }
