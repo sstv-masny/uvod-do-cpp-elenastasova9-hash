218e855eacd81a7188c8b3e3426a5944816b488a
elenastasova9-hash <elenastasova9@gmail.com>
2025-11-07 08:14:54 +0100

    ulož

--- task_06_reverse_digits/main.cpp
+++ task_06_reverse_digits/main.cpp
@@ -13,4 +13,5 @@ int main() {
     } while (n > 0);
 
     cout << cis;
+    return 0;
 }
--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -4,8 +4,9 @@
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    int n; 
-    if (!(std::cin >> n)) return 0;
+    int n;
+    if (!(std::cin >> n))
+        return 0;
     // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
     return 0;
 }
