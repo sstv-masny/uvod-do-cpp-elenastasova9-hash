03da8fe9bc6bba7016d5673d3449a6074bec6536
elenastasova9-hash <elenastasova9@gmail.com>
2025-11-07 08:33:52 +0100

    3

--- task_03_taxi_fare/main.cpp
+++ task_03_taxi_fare/main.cpp
@@ -1,20 +1,24 @@
 using namespace std;
 
-#include <iostream>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     long long k;
-    if (!(std::cin >> k)) return 0;
-    
+    if (!(std::cin >> k))
+        return 0;
+
     double suma = 4.00;
     k = k - 2;
 
     for (long long i = k; i > 0; i--) {
         suma += 1.50;
     }
-    cout << fixed << setprecision(2) << suma << '\n';
+
+    cout << ceil(suma) << '\n'; // ceil = automaticky zaokrúhli nahor a nadol = floor
+
+    // cout << fixed << setprecision(2) << suma << '\n';
     return 0;
 }
