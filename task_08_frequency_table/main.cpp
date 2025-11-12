bf7d1613d5399fa19479a69b7765dc7995712820
Elena <elenastasova9@gmail.com>
2025-11-12 12:25:00 +0100

    8

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -1,12 +1,9 @@
 
 #include <iostream>
 #include <vector>
+
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    int n;
-    if (!(std::cin >> n))
-        return 0;
-    // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
-    return 0;
+
+    int number[] = {1, 2, 3};
+    std::cout << number[0];
 }
