c38cdecb97d0d6ede88d02cc796fb54bd4103843
Elena <elenastasova9@gmail.com>
2025-11-14 08:35:26 +0100

    8 úloha

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -1,9 +1,19 @@
 
 #include <iostream>
+#include <string>
 #include <vector>
+
+using namespace std;
 
 int main() {
 
-    int number[] = {1, 2, 3};
-    std::cout << number[0];
+    int number[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 8, 9};
+
+    int count = sizeof(number) / 4;
+
+    for (int i = 0; i < count; i++) {
+        cout << number[i] << '\n';
+    }
+
+    return 0;
 }
