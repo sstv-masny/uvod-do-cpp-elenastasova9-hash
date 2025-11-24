9f67a47341d1161d5304f30e1923665fa019befa
elenastasova9-hash <elenastasova9@gmail.com>
2025-11-24 19:43:33 +0100

    8 Úloha

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -7,12 +7,21 @@ using namespace std;
 
 int main() {
 
-    int number[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 8, 9};
+    int n;
+    cin >> n;
 
-    int count = sizeof(number) / 4;
+    int pocet[101] = {};
 
-    for (int i = 0; i < count; i++) {
-        cout << number[i] << '\n';
+    for (int i = 0; i < n; i++) {
+        int cislo;
+        cin >> cislo;
+        pocet[cislo]++;
+    }
+
+      for (int i = 0; i <= 100; i++) {
+        if (pocet[i] >0){
+            cout << i << " " << pocet[i] << endl;
+        }
     }
 
     return 0;
